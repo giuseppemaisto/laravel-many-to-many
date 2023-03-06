12e97bcfe54fdcbe2a0115e38232d6813bd7a9bc
giuseppemaisto <gmaisto98@gmail.com>
2023-03-06 14:38:32 +0100

    creato model e migration e seeder per technology

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['HTML', 'CSS', 'Javascript', 'VueJs', 'php', 'laravel'];
+        foreach($technologies as $technology){
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name, '-');
+
+            $newTechnology->save();
+
+        }
+    }
+}
